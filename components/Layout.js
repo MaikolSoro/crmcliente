ac89704f44f94a71e3fd4b8dd56b3f398774bb87
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-08 09:31:05 -0600

    Modificando el head del proyecto y agregando tailwind

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
 
 const Layout = ({children}) => {
 	return (  
 		<>
-			<h1>Desde Layout</h1>
+			<Head>
+				<title>CRM - Administración de Clientes</title>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+				<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+			</Head>
+
+			<Sidebar /> 
 
 			{children}
 		</>
---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => {
+	return ( 
+		<h2>Sidebar</h2>
+	 );
+}
+ 
+export default Sidebar;
