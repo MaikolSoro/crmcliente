cf9096b80475e85a6c4a4d553b7352d13bdf8549
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-08 14:29:55 -0600

    Mostrar un diseño de forma condicional

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+const NuevaCuenta = () => {
+	return ( 
+		<>
+			<Layout>
+				
+				<h1>Nueva Cuenta</h1>
+
+			</Layout>
+		</>
+	 );
+}
+ 
+export default NuevaCuenta;
--- pages/productos.js
+++ pages/productos.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 const Productos = () => (
   <div>
-    <Layout>
-		<h1 className="text-2xl text-gray-800 font-light">Productos</h1>
-    </Layout>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+      </Layout>
   </div>
 )
 
