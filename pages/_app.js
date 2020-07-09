bfff3e6218baa5d3ac0142cb5afde5a91310fb29
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-09 16:16:47 -0600

    Instalando y configurando Apollo dev tools

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import { ApolloProvider } from '@apollo/client';
+import client from '../config/apollo';
+
 const MyApp = ({ Component, pageProps}) => {
-	
+
 	return(
-		
-		<Component {... pageProps} />
+		<ApolloProvider client={client}>
+			<Component {... pageProps} />
+		</ApolloProvider>
 	)
 }
 
