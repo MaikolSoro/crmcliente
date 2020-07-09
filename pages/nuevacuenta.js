a727226c773f6884948bf6e277111adf5f9b48dd
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-09 10:51:37 -0600

    Añadiendo formik para el manejo de los formularios

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 const NuevaCuenta = () => {
+
+	// Validación del formulario
+	const formik = useFormik({
+		initialValues: {
+			nombre:'',
+			apelido:'',
+			email:'',
+			password:''
+		},
+		onSubmit: valores =>{
+			console.log('enviando');
+		}
+	});
+
 	return ( 
 		<>
 			<Layout>
@@ -8,7 +25,9 @@ const NuevaCuenta = () => {
 				<h1 className="text-center text-2xl text-white font-light">Crear Nueva cuenta</h1>
 				<div className="flex justify-center mt-5">
 					<div className="w-full max-w-sm">
-						<form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+						<form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+							  onSubmit={formik.handleSubmit}							
+							>
 
 							<div className="mb-4">
 								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
@@ -18,6 +37,8 @@ const NuevaCuenta = () => {
 									id="nombre"
 									type="text"
 									placeholder="Nombre Usuario"
+									value={formik.values.nombre}
+									onChange={formik.handleChange}
 								/>
 							</div>
 
@@ -29,6 +50,8 @@ const NuevaCuenta = () => {
 									id="apellido"
 									type="text"
 									placeholder="Apellido Usuario"
+									value={formik.values.apelido}
+									onChange={formik.handleChange}
 								/>
 							</div>
 							<div className="mb-4">
@@ -39,6 +62,8 @@ const NuevaCuenta = () => {
 									id="email"
 									type="email"
 									placeholder="Email Usuario"
+									value={formik.values.email}
+									onChange={formik.handleChange}
 								/>
 							</div>
 
@@ -50,6 +75,8 @@ const NuevaCuenta = () => {
 									id="password"
 									type="password"
 									placeholder="Password Usuario"
+									value={formik.values.password}
+									onChange={formik.handleChange}
 								/>
 							</div>
 							<input
