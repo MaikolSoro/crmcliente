085f1322ec108d17ef70b8201032c9c9613f3c3b
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-09 10:38:34 -0600

    Creando el formulario para crear nueva cuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -4,9 +4,62 @@ const NuevaCuenta = () => {
 	return ( 
 		<>
 			<Layout>
-				
-				<h1>Nueva Cuenta</h1>
 
+				<h1 className="text-center text-2xl text-white font-light">Crear Nueva cuenta</h1>
+				<div className="flex justify-center mt-5">
+					<div className="w-full max-w-sm">
+						<form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+
+							<div className="mb-4">
+								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+									Nombre
+								</label>
+								<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+									id="nombre"
+									type="text"
+									placeholder="Nombre Usuario"
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+									Apellido
+								</label>
+								<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+									id="apellido"
+									type="text"
+									placeholder="Apellido Usuario"
+								/>
+							</div>
+							<div className="mb-4">
+								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+									Email
+								</label>
+								<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+									id="email"
+									type="email"
+									placeholder="Email Usuario"
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+									Password
+								</label>
+								<input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+									id="password"
+									type="password"
+									placeholder="Password Usuario"
+								/>
+							</div>
+							<input
+								type="submit"
+								className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:cursor-pointer hover:bg-gray-900 "
+								value="Crear cuenta"
+							/>
+						</form>
+					</div>
+				</div>
 			</Layout>
 		</>
 	 );
