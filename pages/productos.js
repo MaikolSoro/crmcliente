dab499362c7e355ba0d18300448e66450212975f
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-12 17:41:26 -0600

    Listando productos

--- pages/productos.js
+++ pages/productos.js
@@ -1,12 +1,62 @@
-import Head from 'next/head'
-import Layout from '../components/Layout'
-const Productos = () => (
-  <div>
-      <Layout>
-        <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
-      </Layout>
-  </div>
-)
+import Layout from '../components/Layout';
+import Producto from '../components/Producto';
+import { gql, useQuery } from '@apollo/client';
+
+/*=============================================
+             Query con Graphql            
+ =============================================*/
+const OBTENER_PRODUCTOS = gql`
+
+query obtenerProductos{
+  	obtenerProductos {
+      id
+      nombre
+      precio
+      existencia
+    }
+}
+
+`;
+
+
+const Productos = () => {
+
+  // consultar los productos
+
+  const { data, loading, error } = useQuery(OBTENER_PRODUCTOS);
+ 
+  if(loading) return 'Cargando...';
+
+  return (
+    <div>
+        <Layout>
+          <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+
+            <table className="table-auto shadow-md mt-10 w-full w-lg">
+              <thead className="bg-gray-800">
+                <tr className="text-white">
+                  <th className="w-1/5 py-2">Nombre</th>
+                  <th className="w-1/5 py-2">Existencia</th>
+                  <th className="w-1/5 py-2">Precio</th>
+                  <th className="w-1/5 py-2">Eliminar</th>
+                  <th className="w-1/5 py-2">Editar</th>
+                </tr>
+              </thead>
+
+              <tbody className="bg-white">
+                {data.obtenerProductos.map(producto => (
+                  <Producto 
+                  key={producto.id}
+                  producto={producto}
+                  />
+                ))}
+              </tbody>
+            </table>
+
+        </Layout>
+    </div>
+  );
+}
 
 export default Productos
 
