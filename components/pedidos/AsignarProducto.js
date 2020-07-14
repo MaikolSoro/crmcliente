1eb396cd9f174aaf25c2a5d60489065d90789a55
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-14 11:00:32 -0600

    Agregando el componente para Asignar Productos

---
+++ components/pedidos/AsignarProducto.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Select from 'react-select';
+import { gql, useQuery } from '@apollo/client';
+
+const OBTENER_PRODUCTOS = gql`
+	query obtenerProductos{
+		obtenerProductos {
+			id
+			nombre
+			precio
+			existencia
+		}
+   }
+`;
+
+	
+const AsignarProductos = () => {
+
+	//consulta a la base de datos
+
+	const { data, loading, error, } = useQuery(OBTENER_PRODUCTOS);
+
+	console.log(data);	
+
+	if(loading) return null;
+	const { obtenerProductos } = data;
+	return ( 
+		<>
+			<p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-2 text-sm font-bold">2. - Selecciona o busca los productos</p>
+			<Select 
+				className="mt-3"
+				 options={ obtenerProductos }
+				onChange={ opcion => seleccionarCliente(opcion) }
+				getOptionValue={ opciones => opciones.id }
+				getOptionLabel={ opciones => `${opciones.nombre} - ${opciones.existencia} Disponibles` }
+				placeholder= "Busque o Seleccionar el cliente"
+				noOptionsMessage={() => "No hay resultados"}
+			/>
+		</> 
+	 );
+}
+ 
+export default AsignarProductos;
