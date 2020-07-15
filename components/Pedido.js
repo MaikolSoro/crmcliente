e504e663b4acba117c0e2a5b1769318edf94168d
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-15 16:01:52 -0600

    Creando el query y componente pedidos

---
+++ components/Pedido.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pedido = ({pedido}) => {
+	return ( 
+		<h1>{pedido.total}</h1>
+	 );
+}
+ 
+export default Pedido;
--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,16 +1,64 @@
 import Layout from '../components/Layout';
+import Pedido from '../components/Pedido';
 import Link from  'next/link';
+import { gql, useQuery } from '@apollo/client';
 
-const Pedidos = () => (
-  <div>
-    <Layout>
-		<h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
-      <Link  href="/nuevopedido">
-      <a className="bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
-      </Link>
-    </Layout>
-  </div>
-)
+const OBTENER_PEDIDOS = gql`
+  query obtenerPedidosVendedor {
+    obtenerPedidosVendedor {
+        id
+        pedido {
+          id
+          cantidad
+          nombre
+        }
+        cliente {
+          id
+          nombre
+          apellido
+          email
+          telefono
+        }
+        vendedor
+        total
+        estado
+      }
+  }
+`;
+
+
+const Pedidos = () => {
+
+  const { data, loading, error} = useQuery(OBTENER_PEDIDOS);
+
+   if(loading) return 'Cargando....';
+
+   const {obtenerPedidosVendedor} = data;
+    
+    // console.log(data);
+    // console.log(loading);
+    // console.log(error);
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+        <Link  href="/nuevopedido">
+          <a className="bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">Nuevo Pedido</a>
+        </Link>
+          { obtenerPedidosVendedor.length === 0 ? (
+              <p className="mt-5 text-center text-2xl">No hay pedidos aún</p>
+            ) : (
+              obtenerPedidosVendedor.map( pedido => (
+                  <Pedido 
+                    key={pedido.id}
+                    pedido={pedido}
+                  />
+              ))  
+            )}
+      </Layout>
+    </div>
+  );
+}
 
 export default Pedidos
 
