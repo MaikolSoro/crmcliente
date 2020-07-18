07a85406ecb18f3449dfaaabbdafbc540c58e9d9
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-18 15:57:41 -0600

    Creando la página para mejores vendedores

---
+++ pages/mejoresvendedores.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const MejoresVendedores = () => {
+	return (  
+		<Layout>
+			<h1 className="text-2xl-gray-800 font-smaller">Mejores Vendedores</h1>
+		</Layout>
+	);
+}
+ 
+export default MejoresVendedores;
