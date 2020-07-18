d9b62b403811d36d46a470c0bacbeb512be918dd
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-18 16:37:56 -0600

    Graficando los mejores vendedores

--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -4,31 +4,35 @@ import {
 	BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   } from 'recharts';
 
-  const data = [
-	{
-	  name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-	},
-	{
-	  name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-	},
-	{
-	  name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-	},
-	{
-	  name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-	},
-	{
-	  name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-	},
-	{
-	  name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-	},
-	{
-	  name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-	},
-  ];
+  import { gql, useQuery } from '@apollo/client';
+
+  const MEJORES_VENDEDORES = gql`
+	query mejoresVendedores {
+		mejoresVendedores {
+			vendedor {
+				nombre
+				email
+			}
+			total
+		}
+	}
+  `;
   
 const MejoresVendedores = () => {
+
+	const { data, loading, error } = useQuery(MEJORES_VENDEDORES);
+	
+	if(loading) return 'cargando...';
+
+		const { mejoresVendedores } = data;
+		const vendedorGrafica = [];
+
+		mejoresVendedores.map((vendedor, index) => {
+			vendedorGrafica[index] = {
+				...vendedor.vendedor[0],
+				total: vendedor.total
+			}
+		});	
 	return (  
 		<Layout>
 			<h1 className="text-2xl-gray-800 font-smaller">Mejores Vendedores</h1>
@@ -36,17 +40,17 @@ const MejoresVendedores = () => {
 				className="mt-10"
 				width={600}
 				height={500}
-				data={data}
+				data={vendedorGrafica}
 				margin={{
 					top: 5, right: 30, left: 20, bottom: 5,
 				}}
       		>
 				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name" />
+				<XAxis dataKey="nombre" />
 				<YAxis />
 				<Tooltip />
 				<Legend />
-				<Bar dataKey="pv" fill="#3182CE" />
+				<Bar dataKey="total" fill="#3182CE" />
 			</BarChart>
 		</Layout>
 	);
