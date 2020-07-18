b2838be8e7ca01f0d09ea31ff6da2846281c62ac
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-18 15:19:33 -0600

    Actualizar el listado  de pedidos al eliminar

--- components/Pedido.js
+++ components/Pedido.js
@@ -15,13 +15,33 @@ const ELIMINAR_PEDIDO = gql`
 	eliminarPedido(id: $id)
 	}
 `;
+
+const OBTENER_PEDIDOS = gql`
+  query obtenerPedidosVendedor {
+		obtenerPedidosVendedor {
+			id
+		}
+  	}
+`;
 const Pedido = ({pedido}) => {
 
 	const { id, total, cliente: { nombre, apellido, telefono, email }, estado, cliente } = pedido;
 
 	// Mutation para cambiar el estado de un pedido
 	const [ actualizarPedido ] = useMutation(ACTUALIZAR_PEDIDO);
-	const [ eliminarPedido ] = useMutation(ELIMINAR_PEDIDO);
+	const [ eliminarPedido ] = useMutation(ELIMINAR_PEDIDO, {
+		update(cache) {
+			const { obtenerPedidosVendedor } = cache.readQuery({
+				query: OBTENER_PEDIDOS
+			});
+			cache.writeQuery({
+				query: OBTENER_PEDIDOS,
+				data: {
+					obtenerPedidosVendedor: obtenerPedidosVendedor.filter( pedido => pedido.id != id)
+				}
+			})
+		}
+	});
 
 	const [estadoPedido, setEstadoPedido ] = useState(estado);
 	const [ clase, setClase ] = useState('');
@@ -86,7 +106,7 @@ const Pedido = ({pedido}) => {
 						data.eliminarPedido,
 						'success'
 					);
-					
+
 				} catch (error) {
 					console.log(error);
 				}
